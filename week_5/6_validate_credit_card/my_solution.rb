4d4f7b583799ed874c07eccbb44ae67a60ae039e
Matthew Hein <mvernhein@gmail.com>
2014-05-07 11:04:25 -0500

    week 5 credit card solution

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -15,6 +15,17 @@
 # Don't forget to check on intialization for a card length
 # of exactly 16 digits
 class CreditCard
+	def initialize(number)
+		@number = number
+	end
+
+	def check_card
+		valid = false
+		arr = @number.split("")
+		arr.each {|index| arr[index] = arr[index]*2 if index%2 != 0}
+		valid = true if arr.inject{|sum,index| sum + index} % 10 == 0
+		
+	end
 
 end
 
