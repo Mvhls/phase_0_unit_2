203ae03c567cdee01bda1c65fc811b179998a4f9
Matthew Hein <mvernhein@gmail.com>
2014-05-07 17:36:53 -0500

    Week 6 boggle board: added solution

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -5,14 +5,33 @@
 
 
 # 2. Pseudocode
-
+=begin 
+take boggle board challenge from week 5 and paste them inside class
+create initialize method that takes board as argument
+create a class variable and set equal to board
+everywhere board is used, switch with class variable
+take board argument out of every method
+=end
 
 
 # 3. Initial Solution
 class BoggleBoard
- 
-  #your code here
- 
+	def initialize(board)
+		@board = board
+	end
+	def create_word(*coords)
+		coords.map { |coord| @board[coord.first][coord.last]}.join("")
+# [1,2], [1,1], [2,1], [3,2]
+# => "dock"
+	end
+	def get_row(row)
+		return @board[row]
+	end
+	def get_col(col)
+		@board.map {|row| row[col]}
+# => Row: 0:brae, 1:iodt, 2:eclr, 3:take, Column: 0:biet, 1:roca, 
+# 2:adlk, 3:etre
+	end
 end
  
  
@@ -21,12 +40,25 @@ dice_grid = [["b", "r", "a", "e"],
              ["e", "c", "l", "r"],
              ["t", "a", "k", "e"]]
  
-boggle_board = BoggleBoard.new(dice_grid)
+
  
 
 
 # 4. Refactored Solution
-
+class BoggleBoard
+	def initialize(board)
+		@board = board
+	end
+	def create_word(*coords)
+		coords.map { |coord| @board[coord.first][coord.last]}.join("")
+	end
+	def get_row(row)
+		return @board[row]
+	end
+	def get_col(col)
+		@board.map {|row| row[col]}
+	end
+end
 
 
 
@@ -35,6 +67,16 @@ boggle_board = BoggleBoard.new(dice_grid)
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # create driver test code to retrieve a value at a coordinate here:
 # implement tests for each of the methods here:
-
-
+boggle_board = BoggleBoard.new(dice_grid)
+boggle_board.create_word([2, 1], [1, 1], [2, 2], [1, 2]) == "cold"
+boggle_board.get_row(2) == ["e", "c", "l", "r"]
+boggle_board.get_col(1) == ["r", "o", "c", "a"]
+boggle_board.create_word([3, 2]) == "k"
 # 5. Reflection 
+=begin 
+Pretty simple to change into a class. I feel It would of been more 
+simple if it started out as a class. I like using the object oriented 
+approach. Calling the methods on the board are easier, as you don't need 
+to keep adding the board argument every time you want to spell out a word. 
+You can create and name a boggle board whatever you want. 
+=end
