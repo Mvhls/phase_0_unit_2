6c960a5009cda4f74a387d016e4a8babacf261dc
Matthew Hein <mvernhein@gmail.com>
2014-05-07 20:03:28 -0500

    week 6 drawer debugger: solution

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -8,56 +8,62 @@
 
 class Drawer
 
-attr_reader :contents
+  attr_reader :contents
 
 # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+  def initialize
+    @contents = []
+    @open = true
+  end
+
+  def open
+    @open = true
+  end
+
+  def close
+    @open = false
+  end 
+
+  def add_item(item)
+    @contents << item
+    puts "added #{item} to the drawer"
+  end
+
+  def remove_item(item = @contents.pop) #what is `#pop` doing?
+    @contents.delete(item)
+  end
+
+  def dump  # what should this method return?
+    @contents =[]
+    puts "Your drawer is empty."
+  end
+
+  def view_contents
+    puts "The drawer contains:"
+    @contents.each {|silverware| puts "- " + silverware.type }
+  end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+  attr_reader :type
 
 # Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+  def initialize(type, clean = true)
+    @type = type
+    @clean = clean
+  end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
+  def eat
+    puts "eating with the #{type}"
+    @clean = false
+  end
+  
+  def clean_silverware
+    @clean = true
+  end
+  
 end
 
 knife1 = Silverware.new("knife")
@@ -84,16 +90,27 @@ removed_knife.clean_silverware
 silverware_drawer.view_contents
 silverware_drawer.dump
 silverware_drawer.view_contents #What should this return?
+fork = Silverware.new("fork")
+silverware_drawer.add_item(fork) 
+silverware_drawer.view_contents
 
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-fork.eat
+removed_fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code
+
+removed_fork.eat
 
 #BONUS SECTION
 # puts fork.clean
 
 # DRIVER TESTS GO BELOW THIS LINE
+def assert
+  raise "Assertion failed!" unless yield
+end
+assert {silverware_drawer.contents == []}
 
+plastic_knife = Silverware.new("plastic_knife")
+silverware_drawer.add_item(plastic_knife)
+assert {silverware_drawer.contents == [plastic_knife]}
 
 
 
