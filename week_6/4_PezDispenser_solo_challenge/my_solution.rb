0922ffe64f035f1c6b3a7047268db03fa3a8719a
Matthew Hein <mvernhein@gmail.com>
2014-05-09 14:41:43 -0500

    week 6 pez dispenser: solution

--- week_6/4_PezDispenser_solo_challenge/my_solution.rb
+++ week_6/4_PezDispenser_solo_challenge/my_solution.rb
@@ -15,28 +15,84 @@
 
 
 # 2. Pseudocode
+=begin 
+define a class called PezDispenser
+define a method called initialize
+assign instance variables:
+  @flavors = flavors
 
+create a class method called pez_count
+  pez_count has no arguments
+  returns the length of the @flavors array
+
+define a method called see_all_pez
+see_all_pe has no arguments
+puts each index in @flavors array
+
+define a method called add_pez
+add_pez has one argument called flavor
+assign instance variable @flavor = flavor
+pushes @falvor to the end of @flavors array
+
+define a method called get_pez
+pop @flavors
+=end
 
 
 # 3. Initial Solution
 
 class PezDispenser
- 
-# your code here!
- 
+	def initialize(flavors)
+		@flavors = flavors
+	end
+
+	def pez_count
+		return @flavors.length
+	end
+
+	def see_all_pez
+		@flavors.reverse.each {|pez| pez}
+	end
+
+	def add_pez(flavor)
+		@flavor = flavor
+		@flavors.push(@flavor)
+	end
+
+	def get_pez
+		return @flavors.pop
+	end
 end
  
 
 
 # 4. Refactored Solution
+class PezDispenser
+	def initialize(flavors)
+		@flavors = flavors
+	end
 
+	def pez_count
+		@flavors.length
+	end
 
+	def see_all_pez
+		@flavors.sort
+	end
 
+	def add_pez(flavor)
+		@flavors.unshift(flavor)
+	end
 
-
+	def get_pez
+		@flavors.shift
+	end
+end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-
+def assert
+  raise "Assertion failed!" unless yield
+end
 flavors = %w(cherry chocolate cola grape lemon orange peppermint raspberry strawberry).shuffle
 super_mario = PezDispenser.new(flavors)
 puts "A new pez dispenser has been created. You have #{super_mario.pez_count} pez!"  
@@ -53,3 +109,8 @@ puts "Now you have #{super_mario.pez_count} pez!"
 
 
 # 5. Reflection 
+=begin 
+This challenge was very straight foward. I dod not have many 
+problems because I was already used to classes in ruby. This was 
+a good review of everything I learned in ruby so far.
+=end
