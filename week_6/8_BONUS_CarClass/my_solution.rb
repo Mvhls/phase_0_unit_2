c3b8da4385e485b1c49bdc48362b328eb198fe89
Matthew Hein <mvernhein@gmail.com>
2014-05-11 22:22:58 -0500

    week 6 car class: unfinished solution

--- week_6/8_BONUS_CarClass/my_solution.rb
+++ week_6/8_BONUS_CarClass/my_solution.rb
@@ -33,19 +33,54 @@ create a method called time with one argument
 
 # 3. Initial Solution
 class Car
-	def initialize(model, color)
-		@model = model
+    def initialize(model, color)
+    	@model = model
 		@color = color
 		@log = []
+        @north = "north"
+        @south = "south"
+        @east = "east"
+        @west = "west"
+        @direction = @north
+        @time = 0
+        @log.push(@direction)
+        puts "The #{@model} is facing #{@direction} and not moving"
 	end
 	def speed(mph)
 		@car_speed = mph
+		puts "The #{@model} is going #{@car_speed} miles per hour."
+		@log.push(@car_speed)
 	end
+	def turn(direction)
+		@direction = @north if direction == "left" && @direction == @east || direction == "right" && @direction == @west
+		@direction = @west if direction == "left" && @direction == @north || direction == "right" && @direction == @south
+		@direction = @south if direction == "left" && @direction == @west || direction == "right" && @direction == @east
+		@direction = @east if direction == "left" && @direction == @south || direction == "right" && @direction == @north
+		puts "The #{@model} turned #{direction} and is heading #{@direction}"
+		@log.push(direction)
+		@log.push(@direction)
+	end
+	def time(minutes)
+		@distance = (@car_speed * minutes / 60).to_f
+		@time += minutes
+		puts "The #{@model} traveled #{@distance} miles #{@direction} in #{minutes} minutes."
+	end
+	def log
+		puts "Driving History:"
+		@log.each do |x|
+			if x.is_a? Float
+				puts "The #{@model} traveled " + @log[x].to_s + " miles."
+			elsif x.is_a? Integer 
+				puts "The #{@model} is traveilng at " + @log[x].to_s + " miles per hour."
+			elsif x == @north || @south || @east || @west
+				puts "The #{@model} is heading " + @log[x] + "."
+			elsif x == "left" || "right" 
+				puts "The #{@model} turned " + @log[x] + "."
+			end
+		end
+	end
+
 end
-
-
-
-
 
 # 4. Refactored Solution
 
